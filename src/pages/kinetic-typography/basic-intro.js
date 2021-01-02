368f1a0b2e52c2ab118b257906d32a3d55efbb53
Jihee Hwang <alarmringing@gmail.com>
2021-01-01 17:22:07 -0800

    Text on torus completed. Torus looks weird but this seems to be a fault of its geometry implementation itself, not the text texture.

--- src/pages/kinetic-typography/basic-intro.js
+++ src/pages/kinetic-typography/basic-intro.js
@@ -12,11 +12,15 @@ global.THREE = require('three')
 const THREE = global.THREE
 const createGeometry = require("three-bmfont-text");
 
+const screenBkgColor =   0xBE80FF
+const textBkgColor = 0xCCFF00 
+const textColor = 0xFF0066
+
 const useStore = create(set => ({
-  word: 'Lots of text',
+  word: 'NEW YEAR, NEW DAYS',
   position: [-0.965, 0, 0],
   rotation: [Math.PI, 0, 0],
-  zoom: 2.5,
+  zoom: 100,
 }))
 
 const Scene = () => {
@@ -27,11 +31,11 @@ const Scene = () => {
 	useEffect(() => {
 		let scene = new THREE.Scene()
 
-		let camera = new THREE.PerspectiveCamera(75, window.innerWidth, window.innerHeight, 0.1, 1000)
+		let camera = new THREE.PerspectiveCamera(75, mount.current.clientWidth, mount.current.clientHeight, 0.1, 1000)
 		camera.position.z = state.zoom
 
 		let renderer = new THREE.WebGLRenderer({antialias: true})
-		renderer.setClearColor('#995599')
+		renderer.setClearColor(screenBkgColor)
 		renderer.setSize(mount.current.clientWidth, mount.current.clientHeight)
 		renderer.setPixelRatio(mount.current.devicePixelRatio)
 		mount.current.appendChild(renderer.domElement)
@@ -52,27 +56,27 @@ const Scene = () => {
 		// Create render target
 		let rt = new THREE.WebGLRenderTarget(mount.current.clientWidth, mount.current.clientHeight);
 		let rtCamera = new THREE.PerspectiveCamera(45, 1, 0.1, 1000)
-		rtCamera.position.z = 2.5
+		rtCamera.position.z = 4.5
 
 		let rtScene = new THREE.Scene()
-		rtScene.background = new THREE.Color("#FFFFFF")
+		rtScene.background = new THREE.Color(textBkgColor)
 
 
 		// load font files
 		let loadBMF = () => {
 			let geometry
-			loadFont('fonts/GmarketSansMedium.fnt', (err, font) => {
+			loadFont('fonts/Lato-Black.fnt', (err, font) => {
 				let fontGeometry = createGeometry({
 					font: font,
 					text: state.word
 				})
 				let loader = new THREE.TextureLoader()
-				loader.load(`fonts/GmarketSansMedium.png`, (texture) => {
+				loader.load(`fonts/Lato-Black.png`, (texture) => {
 					setTimeout(() => {
 						let fontMaterial = new THREE.RawShaderMaterial(
 												MSDFShader({
 													map: texture,
-													color: 0x000000,
+													color: textColor,
 													side: THREE.DoubleSide,
 													transparent: true,
 													negate: false
@@ -92,7 +96,7 @@ const Scene = () => {
 		let populateRenderTarget = (fontGeometry, fontMaterial) => {
 			let textMesh = new THREE.Mesh(fontGeometry, fontMaterial)
 
-			textMesh.position.set(-0.965, -0.275, 0)
+			textMesh.position.set(-1.6, -0.275, 0)
 			textMesh.rotation.set(Math.PI, 0, 0)
 			textMesh.scale.set(0.008, 0.02, 1)
 
@@ -101,16 +105,21 @@ const Scene = () => {
 
 		let mesh, geometry, material
 		let createSceneMesh = () => {
-			geometry = new THREE.BoxGeometry(1, 1, 1);
+			//geometry = new THREE.BoxGeometry(1, 1, 1); // Cube
+			geometry = new THREE.TorusKnotGeometry(10, 3, 100, 16 );
+
 			material = new THREE.ShaderMaterial({
 				vertexShader:vert,
 				fragmentShader:frag,
+				transparent: true,
 				uniforms: {
 					uTime: {value: 0},
 					uTexture: {value: rt.texture}
 				}
 			})
+
 			mesh = new THREE.Mesh(geometry, material)
+			// scene.add(new THREE.DirectionalLight( 0xffffff, 0.5 ));
 			scene.add(mesh)
 		}
 
@@ -143,8 +152,8 @@ const Scene = () => {
 			controls.update()
 			material.uniforms.uTime.value = clock.getElapsedTime()
 
-			mesh.rotation.x += 0.005
-			mesh.rotation.z += 0.005 
+			mesh.rotation.x += 0.004
+			mesh.rotation.z += 0.006
 
 			renderer.setRenderTarget(rt)
 			renderer.render(rtScene, rtCamera)
--- src/pages/kinetic-typography/shaders.js
+++ src/pages/kinetic-typography/shaders.js
@@ -1,25 +1,31 @@
 const vert = `
 	out vec2 vUv;
+	out vec3 vPos;
 	void main() {
 		vUv = uv;
+		vPos = position;
 		gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.);
 	}
 `
 
 const frag = `
 	in vec2 vUv;
+	in vec3 vPos;
 	uniform sampler2D uTexture;
+	//uniform vec4 uBkgColor;
 	uniform float uTime;
 
 	void main() {
-		float time = uTime * 0.75;
-		vec2 repeat = vec2(2., 6.);
-		vec2 uv = fract(vUv * repeat + vec2(-time, 0));
+		float time = uTime * 0.2;
+		vec2 repeat = vec2(5., 4.);
+		vec2 uv = fract(vUv * repeat + vec2(time, 0));
+
+		float shadow = pow(clamp(clamp(vPos.z / 5., 0., 1.)+0.1, 0., 1.), 0.3);
 
 		vec3 val = texture(uTexture, uv).rgb;
-		// val *= vec3(uv.x, uv.y, 1.); // This is to test if UVs are correct.
+		//val *= vec3(uv.x, uv.y, 1.); // This is to test if UVs are correct.
 
-		gl_FragColor = vec4(1.-val, 1.); // Inverts.
+		gl_FragColor = vec4(val*shadow, 1.); // Inverts.
 	}
 `
 
